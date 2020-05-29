dd6b93bb5544b46bc0c226fad580e59ecddf5372
Cahyo Dwinov <59456940+cahyo-dwinov@users.noreply.github.com>
2020-05-29 21:37:38 +0700

    Belajar Rest Parameter

--- practice/practice.js
+++ practice/practice.js
@@ -21,3 +21,19 @@ const allFavorites = [...favorites, ...others];
 console.log(allFavorites);
 
 
+//Rest Parameter
+// function sum(...numbers) {
+//     var result = 0;
+//     for(let number of numbers) {
+//         result += numbers
+//     }
+//     return result;
+// }
+// console.log(sum(1, 2, 3, 4, 5));
+
+//REST PARAMETER FOR ARRAY DESTRUCTURING
+const refrigerator = ['Samsung', 50, 2, 'Milk', 'Cheese', 'Egg', 'Butter'];
+const [manufacture, wight, door, ...items] = refrigerator;
+console.log(manufacture);
+console.log(door);
+console.log(items);
